99c0834f0d53c916cda34039c8e408c523cc22a9
Tony Di Nucci <tony.dinucci@gmail.com>
2019-04-15 23:46:39 +0100

    Clear out initial draft

--- test/register-test.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "../src/register/register.h"
-
-TEST(Construct, Equals) {
-    auto reg = new Register<uint16_t>("PC");
-
-    EXPECT_EQ("PC", reg->get_name());
-}
